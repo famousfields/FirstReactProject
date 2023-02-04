93da9b431d920b69d49dcc9b474d4197963b06a6
famousfields <willfields92@gmail.com>
2023-02-03 22:26:49 -0500

    add: post request

--- src/Create.js
+++ src/Create.js
@@ -1,39 +1,52 @@
 import { useState } from "react";
 
 const Create = () => {
-    
-    const [title, setTitle] = useState('');
-    const[body, setBody] =useState('');
-    const[author,setAuthor] = useState('William');
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('William');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const blog = {title,body,author};
-        console.out(blog);
-    }
-    
-    return ( 
-        <div className="create">
-            <h2>Add a new blog</h2>
-            <form onSubmit={handleSubmit}>
-                <label>Blog title</label>
-                <input 
-                    type = "text" 
-                    required
-                />
-                <label>Blog body:</label>
-                <textarea 
-                    required
-                />
-                <label>Blog author:</label>
-                <select>
-                    <option value = "William">William</option>
-                    <option value = "Derrick">Derrick</option>
-                </select>
-                <button>Add blog</button>
-            </form>
-        </div>
-     );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    fetch('http://localhost:8000/blogs/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog)
+    }).then(() => {
+      console.log('new blog added');
+    })
+  }
+
+  return (
+    <div className="create">
+      <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input 
+          type="text" 
+          required 
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <select
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="William">William</option>
+          <option value="Derrick">Derrick</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
+    </div>
+  );
 }
  
 export default Create;
