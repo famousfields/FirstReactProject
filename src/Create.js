960ea87ed016abc66ba08bd337a1a2963f2a7d49
famousfields <willfields92@gmail.com>
2023-02-03 21:59:46 -0500

    add: forms

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,37 @@
+import { useState } from "react";
+
 const Create = () => {
+    
+    const [title, setTitle] = useState('');
+    const[body, setBody] =useState('');
+    const[author,setAuthor] = useState('William');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = {title,body,author};
+        console.out(blog);
+    }
+    
     return ( 
-        <div className="Create">
+        <div className="create">
             <h2>Add a new blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog title</label>
+                <input 
+                    type = "text" 
+                    required
+                />
+                <label>Blog body:</label>
+                <textarea 
+                    required
+                />
+                <label>Blog author:</label>
+                <select>
+                    <option value = "William">William</option>
+                    <option value = "Derrick">Derrick</option>
+                </select>
+                <button>Add blog</button>
+            </form>
         </div>
      );
 }
