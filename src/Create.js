e67890ce4666b5140d80a7809e185322132b9175
famousfields <willfields92@gmail.com>
2023-02-04 19:29:04 -0500

    add: useHistory

--- src/Create.js
+++ src/Create.js
@@ -1,20 +1,28 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('William');
+  const [ isPending, setIsPending] = useState(false);
+  const history = useHistory();//represents history(back and forward arrows)
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
 
+    setIsPending(true);
+
     fetch('http://localhost:8000/blogs/', {
-      method: 'POST',
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(blog)
+      method: 'POST',// tells server that we are sending an object
+      headers: { "Content-Type": "application/json" },// tells server what type of data is being sent
+      body: JSON.stringify(blog)// turns object into a json string
     }).then(() => {
       console.log('new blog added');
+      setIsPending(false);
+     // history.go(-1); goes back one page
+     history.push('/');// takes us back to home page usin its route
     })
   }
 
@@ -43,7 +51,8 @@ const Create = () => {
           <option value="William">William</option>
           <option value="Derrick">Derrick</option>
         </select>
-        <button>Add Blog</button>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button>Adding blog...</button>}
       </form>
     </div>
   );
